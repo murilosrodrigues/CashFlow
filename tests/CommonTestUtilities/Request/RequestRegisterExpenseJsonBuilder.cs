2e47ea9f1a8b9aad5712fa38a8be7d908206ec2a
murilosrodrigues <murilorodrigues29@gmail.com>
2024-05-27 07:46:12 -0300

    implementado testes unitarios para os validators

---
+++ tests/CommonTestUtilities/Request/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Request;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommonTestUtilities.Request;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+
+    public static RequestRegisterExpenseJson Build()
+    {
+       return  new Faker<RequestRegisterExpenseJson>()
+             .RuleFor(r => r.Title, Faker => Faker.Commerce.ProductName())
+             .RuleFor(r => r.Description, Faker => Faker.Commerce.ProductDescription())
+             .RuleFor(r => r.Date, Faker => Faker.Date.Past())
+             .RuleFor(r => r.PaymenteType, Faker => Faker.PickRandom<PaymentType>())
+             .RuleFor(r => r.Amount, Faker => Faker.Random.Decimal(min: 1, max: 1000));
+    }
+
+}
